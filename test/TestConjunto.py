176e3cebf0b3dd0f8d71f1f70f5f06f170e97dd3
yohan316 <71734556@continental.edu.pe>
2025-04-23 11:30:57 -0500

    Caso 2: Calcular promedio con un conjunto de un elemento (Roo)

--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -1,11 +1,14 @@
 import unittest
-from src.logica.conjunto import
+from src.logica.conjunto import Conjunto  # Asegúrate de que esta ruta sea correcta
 
-class TestConjunto( unittest.TestCase ):
+class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
-            self.assertIsNone(conjunto.promedio())
+        self.assertIsNone(conjunto.promedio())
 
-    def test_conjunto_unElemento_retornaValorUnicoElemento( self ):
-        conjunto=Conjunto([1])
-            self.assertEqual(5,conjunto.promedio())
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(5, conjunto.promedio())
+
+if __name__ == '__main__':
+    unittest.main()
