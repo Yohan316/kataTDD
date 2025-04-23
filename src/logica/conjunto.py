09e4e21db37b7d1da645bd4d85b8416322c34996
yohan316 <71734556@continental.edu.pe>
2025-04-23 11:24:39 -0500

    Caso 1: Calcular promedio con conjunto vacío (Verde)

--- src/logica/conjunto.py
+++ src/logica/conjunto.py
@@ -1,6 +1,5 @@
 class Conjunto:
     def __init__(self, conjunto):
-        self.__conjunto = conjunto
-
-    def promedio(self):
-      return False
+        self.__conjunto=conjunto
+    def promedio( self ):
+         return None
